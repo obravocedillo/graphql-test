48cd0d3ba53c31b5e112436f96d9c678076466d8
Oliver Brandon Bravo Cedillo <oliver.bravoc@gmail.com>
2021-05-06 12:07:38 -0500

    feat: add book mutation added

--- src/context/MainProvider.jsx
+++ src/context/MainProvider.jsx
@@ -1,7 +1,10 @@
 import React, { useContext, createContext, useState } from 'react';
 
+const axios = require('axios');
+
 const initialContextValue = {
-  name: 'Oliver',
+  fullName: 'Oliver',
+  books: [],
 };
 
 const MainContext = createContext(initialContextValue);
@@ -15,8 +18,63 @@ function useMainProvider() {
 }
 
 function MainProvider({ children }) {
-  const [name] = useState('Oliver');
-  return <MainContext.Provider value={{ name }}>{children}</MainContext.Provider>;
+  const [fullName] = useState('Oliver');
+  const [books, setBooks] = useState([]);
+  const getAllBooksAxios = async () => {
+    try {
+      const postGetAllBooks = await axios.post(
+        'http://localhost:3002/graphql',
+        {
+          query: `
+            query {
+              getAllBooks {
+                name
+                author
+                year
+              }
+            }
+            `,
+        },
+        {}
+      );
+      setBooks(postGetAllBooks.data.data.getAllBooks);
+      return postGetAllBooks.data.data.getAllBooks;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+  const addNewBook = async (name, author, year) => {
+    const postAddNewBook = await axios.post(
+      'http://localhost:3002/graphql',
+      {
+        query: `
+          mutation saveBook($name:String!, $author:String!, $year:Int!) {
+            saveBook(name:$name, author:$author, year:$year) { 
+              name,
+              author,
+              year
+            }
+          }
+        `,
+        variables: {
+          name,
+          author,
+          year: parseInt(year, 10),
+        },
+      },
+      {}
+    );
+    const currentBooks = [...books];
+    currentBooks.push(postAddNewBook.data.data.saveBook);
+    setBooks(currentBooks);
+    return postAddNewBook.data.data.saveBook;
+  };
+  return (
+    <MainContext.Provider value={{ fullName, books, getAllBooksAxios, addNewBook }}>
+      {children}
+    </MainContext.Provider>
+  );
 }
 
 export default MainProvider;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,75 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
 import { useMainProvider } from '../../context/MainProvider';
 import './Home.css';
 
-const GET_ALL_BOOKS = gql`
-  query {
-    getAllBooks {
-      name
-      author
-      year
-    }
-  }
-`;
-
 function Home() {
-  const { loading, data } = useQuery(GET_ALL_BOOKS);
-  const { name } = useMainProvider();
-  if (loading) return <h1>Loading</h1>;
+  const { fullName, books, getAllBooksAxios, addNewBook } = useMainProvider();
+  const [nameBook, setNameBook] = useState('');
+  const [authorBook, setAuthorBook] = useState('');
+  const [yearBook, setYearBook] = useState(2021);
+  useEffect(() => {
+    if (books.length === 0) {
+      getAllBooksAxios();
+    }
+    console.log(books);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [books.length]);
+
+  const addNewBookHandler = (event) => {
+    event.preventDefault();
+    addNewBook(nameBook, authorBook, yearBook);
+  };
+
+  const bookNameChangeHandler = (event) => {
+    setNameBook(event.target.value);
+  };
+  const bookAuthorChangeHandler = (event) => {
+    setAuthorBook(event.target.value);
+  };
+  const bookYearChangeHandler = (event) => {
+    setYearBook(event.target.value);
+  };
+  if (books.length === 0) return <h1>Loading</h1>;
   return (
-    <div>
-      <h1>Hello {name}</h1>
-      {data.getAllBooks.map((singleBook) => {
-        return <h2 key={singleBook.name}>{singleBook.name}</h2>;
-      })}
+    <div className="home-main-container">
+      <h1 className="home-title">
+        Hello <strong>{fullName}</strong>
+      </h1>
+      <div className="books-container">
+        {books.map((singleBook) => {
+          return (
+            <div key={singleBook.name} className="single-book">
+              <h3 className="single-book-title">{singleBook.name}</h3>
+              <p className="single-book-author">{singleBook.author}</p>
+              <p className="single-book-year">{singleBook.year}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <input
+        placeholder="Book name"
+        onChange={(e) => bookNameChangeHandler(e)}
+        className="add-book-input"
+      />
+      <input
+        placeholder="Book authro"
+        onChange={(e) => bookAuthorChangeHandler(e)}
+        className="add-book-input"
+      />
+      <input
+        placeholder="Book year"
+        type="number"
+        onChange={(e) => bookYearChangeHandler(e)}
+        className="add-book-input"
+      />
+      <button
+        type="button"
+        onClick={(e) => addNewBookHandler(e)}
+        className="add-book-button"
+      >
+        Save book
+      </button>
     </div>
   );
 }
